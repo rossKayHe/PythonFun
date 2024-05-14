0542118670361025fe72a78f252b7c87cc048f08
rossKayHe <rossKayHe@users.noreply.github.com>
2024-05-14 13:16:47 -0700

    Add files via upload

---
+++ butter.py
@@ -0,0 +1,424 @@
+import pyautogui, time, winsound
+from random import randint
+ # one
+px = pyautogui.pixel(1511,760)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1511,760)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1556,760)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(randint (2,3))
+px = pyautogui.pixel(1556,760)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(randint (2,3))
+
+px = pyautogui.pixel(1596,760)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+    if pyautogui.locateOnScreen('nofire.png',confidence=.6):
+        pyautogui.moveTo(pyautogui.size().width/2-170,pyautogui.size().height/2+20)
+        pyautogui.click() 
+
+px = pyautogui.pixel(1596,760)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+# two
+    #2-1
+px = pyautogui.pixel(1475,795)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1469 + randint(3,5),785 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1475,795)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1469 + randint(3,5),795 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+#2-2
+px = pyautogui.pixel(1511,795)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),795 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1511,795)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),795 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1556,795)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),795 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1556,795)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),795 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1596,795)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),795 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1596,795)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),795 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+# three
+    #3-1
+px = pyautogui.pixel(1475,828)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1469 + randint(3,5),826 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1475,828)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1469 + randint(3,5),826 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+#3-2
+px = pyautogui.pixel(1521,826)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),826 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1521,826)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),826 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+#3-3
+px = pyautogui.pixel(1556,826)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),826 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1556,826)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),826 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+#3-4
+px = pyautogui.pixel(1606,826)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),826 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1606,826)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),826 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+# four
+px = pyautogui.pixel(1469,867)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1469 + randint(3,5),867 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1469,867)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1469 + randint(3,5),867 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1511,867)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),867 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1511,867)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),867 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1556,867)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),867 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1556,867)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),867 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1596,867)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),867 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1596,867)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),867 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+# five
+px = pyautogui.pixel(1469,903)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1469 + randint(3,5),903 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1469,903)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1469 + randint(3,5),903 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1511,903)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),903 + randint(3,5))
+    pyautogui.click() 
+    print("\a")
+    time.sleep(7)
+px = pyautogui.pixel(1511,903)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),903 + randint(3,5))
+    pyautogui.click() 
+    print("\a")
+    time.sleep(7)
+
+px = pyautogui.pixel(1556,903)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),903 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1556,903)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),903 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1596,903)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),903 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1596,903)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),903 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+# six
+px = pyautogui.pixel(1475,938)
+if px[0]!=62 and px[0]!=59:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1469 + randint(3,5),938 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1475,938)
+if px[0]!=62 and px[0]!=59:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1469 + randint(3,5),938 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1521,938)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),938 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1521,938)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),938 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1556,938)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),938 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1556,938)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),938 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1606,938)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),938 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1606,938)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),938 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+# seven
+px = pyautogui.pixel(1475,971)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1469 + randint(3,5),971 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1475,971)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1469 + randint(3,5),971 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1521,971)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),971 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1521,971)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1511 + randint(3,5),971 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1556,971)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),971 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1556,971)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1556 + randint(3,5),971 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+
+px = pyautogui.pixel(1606,971)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),971 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
+px = pyautogui.pixel(1606,971)
+if px[0]!=62 and px[0]!=64:
+    pyautogui.moveTo(1469 + randint(3,5),760 + randint(3,5))
+    pyautogui.click() 
+    pyautogui.moveTo(1596 + randint(3,5),971 + randint(3,5))
+    pyautogui.click() 
+    time.sleep(3)
---
+++ drip.py
@@ -0,0 +1,84 @@
+import pyautogui
+import time
+import random
+
+pyautogui.moveTo(873,8)
+pyautogui.click() 
+pyautogui.keyDown('shiftleft')
+# one
+pyautogui.moveTo(1469,760)
+pyautogui.click() 
+pyautogui.moveTo(1511,760)
+pyautogui.click() 
+pyautogui.moveTo(1556,760)
+pyautogui.click() 
+pyautogui.moveTo(1596,760)
+pyautogui.click() 
+time.sleep(random.randint(1, 9)/ 10)
+
+# two
+pyautogui.moveTo(1469,795)
+pyautogui.click() 
+pyautogui.moveTo(1511,795)
+pyautogui.click() 
+pyautogui.moveTo(1556,795)
+pyautogui.click() 
+pyautogui.moveTo(1596,795)
+pyautogui.click() 
+time.sleep(random.randint(1, 9)/ 10)
+
+# three
+pyautogui.moveTo(1469,826)
+pyautogui.click() 
+pyautogui.moveTo(1511,826)
+pyautogui.click() 
+pyautogui.moveTo(1556,826)
+pyautogui.click() 
+pyautogui.moveTo(1596,826)
+pyautogui.click() 
+time.sleep(random.randint(1, 9)/ 10)
+
+# four
+pyautogui.moveTo(1469,867)
+pyautogui.click() 
+pyautogui.moveTo(1511,867)
+pyautogui.click() 
+pyautogui.moveTo(1556,867)
+pyautogui.click() 
+pyautogui.moveTo(1596,867)
+pyautogui.click() 
+time.sleep(random.randint(1, 9)/ 10)
+
+# five
+pyautogui.moveTo(1469,903)
+pyautogui.click() 
+pyautogui.moveTo(1511,903)
+pyautogui.click() 
+pyautogui.moveTo(1556,903)
+pyautogui.click() 
+pyautogui.moveTo(1596,903)
+pyautogui.click() 
+time.sleep(random.randint(1, 9)/ 10)
+
+# six
+pyautogui.moveTo(1469,938)
+pyautogui.click() 
+pyautogui.moveTo(1511,938)
+pyautogui.click() 
+pyautogui.moveTo(1556,938)
+pyautogui.click() 
+pyautogui.moveTo(1596,938)
+pyautogui.click() 
+time.sleep(random.randint(1, 9)/ 10)
+
+# seven
+pyautogui.moveTo(1469,971)
+pyautogui.click() 
+pyautogui.moveTo(1511,971)
+pyautogui.click() 
+pyautogui.moveTo(1556,971)
+pyautogui.click() 
+pyautogui.moveTo(1596,971)
+pyautogui.click() 
+pyautogui.keyUp('shiftleft')
+time.sleep(random.randint(1, 9)/ 10)
